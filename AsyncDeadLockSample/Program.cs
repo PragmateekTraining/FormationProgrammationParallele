881de6ce55d245b9c07dbca5507b4839f22f90f1
Pragmateek <training@pragmateek.com>
2014-09-21 23:24:25 +0200

    Add async deadlock sample

---
+++ AsyncDeadLockSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncDeadLockSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button OKButton = new Button { Text = "OK!", ForeColor = Color.Green };
+            OKButton.Click += OKButton_Click;
+
+            Button KOButton = new Button { Text = "KO!", ForeColor = Color.Red };
+            KOButton.Click += KOButton_Click;
+
+            Form form = new Form();
+
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+            panel.Controls.Add(OKButton);
+            panel.Controls.Add(KOButton);
+
+            form.Controls.Add(panel);
+
+            Application.Run(form);
+        }
+
+        static async Task WaitOneSecond()
+        {
+            await Task.Delay(1);
+        }
+
+        static async void OKButton_Click(object sender, EventArgs e)
+        {
+            await WaitOneSecond();
+        }
+
+        static void KOButton_Click(object sender, EventArgs e)
+        {
+            WaitOneSecond().Wait();
+        }
+    }
+}
---
+++ AsyncDeadLockSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AsyncDeadLockSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsyncDeadLockSample")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3456af94-db8d-46be-8bc6-ee17d636d3af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
