fb861cc0120374bdbe23ff6cc77986f195d685bd
Pragmateek <training@pragmateek.com>
2014-09-23 00:45:35 +0200

    Add Task.Delay and FromResult sample + cleanup

---
+++ TaskDelaySample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDelaySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            Task<HttpResponseMessage> slowDownloadTask = httpClient.GetAsync("http://download.opensuse.org/distribution/11.0/iso/dvd/openSUSE-11.0-DVD-i386.iso?mirrorlist");
+            Task<HttpResponseMessage> fastDownloadTask = httpClient.GetAsync("http://pragmateek.com");
+
+            Task timeoutTask = Task.Delay(10000);
+
+            Task firstToCompleteTask = Task.WhenAny(fastDownloadTask, timeoutTask).Result;
+
+            if (firstToCompleteTask == fastDownloadTask)
+            {
+                Console.WriteLine("File downloaded.");
+            }
+            else
+            {
+                Console.WriteLine("Download is too long!");
+            }
+        }
+    }
+}
---
+++ TaskFromResultSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFromResultSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<string> t1 = Task.FromResult("Hello");
+
+            TaskCompletionSource<string> tcs = new TaskCompletionSource<string>();
+            tcs.SetResult("World");
+
+            Task<string> t2 = tcs.Task;
+
+            Console.WriteLine(t1.Result);
+            Console.WriteLine(t2.Result);
+        }
+    }
+}
