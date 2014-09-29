c604f124051a297bf28e501be899a4e16423bdd6
Pragmateek <training@pragmateek.com>
2014-09-29 13:29:02 +0200

    Add async initialization pattern sample

--- AsyncInstantiationSample/Program.cs
+++ AsyncInstantiationSample/Program.cs
@@ -8,6 +8,18 @@ namespace AsyncInstantiationSample
 {
     class Internet
     {
+        public Task Initialization { get; private set; }
+
+        public Internet()
+        {
+            Initialization = InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            await Task.Delay(1000);
+        }
+
         public static async Task<Internet> New()
         {
             await Task.Delay(1000);
@@ -20,8 +32,13 @@ namespace AsyncInstantiationSample
     {
         private static async Task GetInternet()
         {
-            Console.WriteLine("[{0:hh:mm:ss.fff}] Loading internet...", DateTime.Now);
+            Console.WriteLine("[{0:hh:mm:ss.fff}] Loading internet with factory...", DateTime.Now);
             Internet internet = await Internet.New();
+            Console.WriteLine("[{0:hh:mm:ss.fff}] Internet loaded.", DateTime.Now);
+
+            Console.WriteLine("[{0:hh:mm:ss.fff}] Loading internet with async initialization pattern...", DateTime.Now);
+            internet = new Internet();
+            await internet.Initialization;
             Console.WriteLine("[{0:hh:mm:ss.fff}] Internet loaded.", DateTime.Now);
         }
 
