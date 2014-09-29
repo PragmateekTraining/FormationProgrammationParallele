76d3dffb6e7d03c4ff7b08a38cd78088a422b306
Pragmateek <training@pragmateek.com>
2014-09-29 12:56:02 +0200

    Add async instantiation sample with factory

---
+++ AsyncInstantiationSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncInstantiationSample
+{
+    class Internet
+    {
+        public static async Task<Internet> New()
+        {
+            await Task.Delay(1000);
+
+            return new Internet();
+        }
+    }
+
+    class Program
+    {
+        private static async Task GetInternet()
+        {
+            Console.WriteLine("[{0:hh:mm:ss.fff}] Loading internet...", DateTime.Now);
+            Internet internet = await Internet.New();
+            Console.WriteLine("[{0:hh:mm:ss.fff}] Internet loaded.", DateTime.Now);
+        }
+
+        static void Main(string[] args)
+        {
+            GetInternet();
+
+            Console.ReadLine();
+        }
+    }
+}
