46db3bf6bd47e3ff7c67c1699ba07b68bdd3760b
Pragmateek <training@pragmateek.com>
2014-09-30 15:30:42 +0200

    Add task continuation WPF sample

---
+++ TaskContinuationWPFSample/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskContinuationWPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int GetAnswer()
+        {
+            return 42;
+        }
+
+        private void OutputAnswer(object answer)
+        {
+            output.Text = answer.ToString();
+        }
+
+        private void ShowError(Exception exception)
+        {
+            MessageBox.Show(exception.ToString());
+        }
+
+        private void badButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task<int>.Run((Func<int>)GetAnswer)
+                     .ContinueWith(task => OutputAnswer(task.Result))
+                     .ContinueWith(task => ShowError(task.Exception), TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        private void goodButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task<int>.Run((Func<int>)GetAnswer)
+                     .ContinueWith(task => OutputAnswer(task.Result), TaskScheduler.FromCurrentSynchronizationContext())
+                     .ContinueWith(task => ShowError(task.Exception), TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
